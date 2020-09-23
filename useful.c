993c8dc822063729a0eb6104b802884521ac01b7
ethan-tayar <ethan.tayar@gmail.com>
2020-09-23 12:44:07 +0300

    change read_line to use fgets instead of scanf

--- useful.c
+++ useful.c
@@ -3,10 +3,13 @@ This file contains useful function in C
 */
 
 #include <stdio.h>
+#include <string.h>
 
-void read_line(char *buf)
+void read_line(char *buf, int size)
 {
-    scanf("%s", buf);
+    fgets(buf, size, stdin);
+    int len = strlen(buf);
+    buf[len-1] = 0;
 }
 
 int main()
@@ -14,7 +17,7 @@ int main()
     char buf[20] = "";
 
     printf("Please enter a string: ");
-    read_line(buf);
+    read_line(buf, 20);
     printf("buf = %s\n", buf);
     printf("DONE\n");
     return 0;
